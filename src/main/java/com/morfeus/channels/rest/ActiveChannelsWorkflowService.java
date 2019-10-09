b10b99c31b773dbc72f5e59ac6eb5636c896fa0b
Pritesh <pritesh.soni@active.ai>
2019-10-09 15:27:37 +0530

    666

--- src/main/java/com/morfeus/channels/rest/ActiveChannelsWorkflowService.java
+++ src/main/java/com/morfeus/channels/rest/ActiveChannelsWorkflowService.java
@@ -135,15 +135,15 @@ public class ActiveChannelsWorkflowService {
     }
     String base = "Your Request for " + selectedCard + " Block card has been";
     String actualTitle = "";
-    if (confirmation.contains("CONFIRM") || confirmation.equalsIgnoreCase("confirm")) {
+    if (confirmation.equalsIgnoreCase("confirm")) {
       actualTitle = base + " blocked successfully.";
     } else {
       actualTitle = base + " cancelled.";
     }
     String image = "";
-    if (selectedCard.contains("AXIS") || selectedCard.equalsIgnoreCase("axis")) {
+    if (selectedCard.equalsIgnoreCase("axis")) {
       image = "https://news.manikarthik.com/wp-content/uploads/Axis-Bank-Platinum-Credit-Card.png";
-    } else if (selectedCard.contains("HDFC") || selectedCard.equalsIgnoreCase("hdfc")) {
+    } else if (selectedCard.equalsIgnoreCase("hdfc")) {
       image = "https://cards.jetprivilege.com/cards/HDFC-Jet-Privilege-World-DI-Card_final-24-10-17-011519069130907.jpg";
     } else {
       image = "https://image3.mouthshut.com/images/imagesp/925006383s.png";
