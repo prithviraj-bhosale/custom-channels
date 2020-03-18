ec4333c9e9cf8a63b5740e16b167158aa8375a12
prithviraj-bhosale <96pbhosale@gmail.com>
2020-03-18 16:16:46 +0530

    changes standalone app

--- src/main/java/com/morfeus/channels/rest/StandAloneApplication.java
+++ src/main/java/com/morfeus/channels/rest/StandAloneApplication.java
@@ -12,7 +12,6 @@ import requestObject.Request;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,8 +32,6 @@ public class StandAloneApplication {
     Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     Request req= objectMapper.readValue(body,Request.class);
     String mobileNumber=req.getTo().getNumber();
-    mobileNumber= "918983726887";
-    redisTemplate.opsForValue().set("918983726887","https://cc842234.ngrok.io/morfeus/v1/channels/24wn43775241160/message");
     try{
       url=redisTemplate.opsForValue().get(mobileNumber);
       if(!url.isEmpty())
@@ -42,57 +39,68 @@ public class StandAloneApplication {
     }
     catch(Exception e)
     {
-      LOGGER.log(Level.SEVERE, (Supplier<String>) e);
+      LOGGER.log(Level.SEVERE, e.toString());
     }
     LOGGER.log(Level.INFO, "END OF ACCEPTREQUEST FUNCTION");
   }
 
-  private void postRequest(String body, String urlToRedirect) throws IOException {
+  private void postRequest(String body,String urlToRedirect) throws IOException {
     OkHttpClient client = new OkHttpClient();
     Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    Response response = null;
+    Response response=null;
     try {
       MediaType mediaType = MediaType.parse("application/json");
       com.squareup.okhttp.RequestBody requestBody = com.squareup.okhttp.RequestBody.create(mediaType, body);
-      com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder().url(urlToRedirect).method("POST", requestBody)
-          .addHeader("Content-Type", "application/json").build();
+      com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder().url(urlToRedirect).method("POST", requestBody).addHeader("Content-Type", "application/json").build();
       response = client.newCall(request).execute();
-      LOGGER.log(Level.INFO, "END OF POSTREQUEST FUNCTION");
-    } catch (IOException e) {
+    }
+    catch (IOException e)
+    {
       System.out.println(e);
       LOGGER.log(Level.SEVERE, (Supplier<String>) e);
-    } finally {
-      if (response != null && response.isSuccessful())
-        response.body().close();
-      else if (response == null)
-        LOGGER.log(Level.INFO, "Failed to post! ");
     }
+    finally{
+      if(response!=null && response.isSuccessful())
+      {
+        response.body().close();
+        LOGGER.log(Level.INFO,"redirected to url---> "+urlToRedirect);
+      }
+      else if(response==null)
+        LOGGER.log(Level.INFO,"Failed to post! ");
+    }
+    LOGGER.log(Level.INFO, "END OF POSTREQUEST FUNCTION");
   }
 
-  @RequestMapping(path = "/morfeus/whatsapp/manage", consumes = "application/json", produces = "application/json")
-  private void addNumber(@RequestBody(required = true) String body, HttpServletResponse httpServletResponse,
-      @RequestParam(value = "number", required = false) String number) {
-    String[] keyValue = body.split(":");
+  @RequestMapping(path = "/morfeus/whatsapp/manage", consumes = "application/json",produces = "application/json")
+  private void addNumber(@RequestBody(required = true) String body,HttpServletResponse httpServletResponse,
+      @RequestParam(value = "number",required = false) String number)
+  {
+    String[] keyValue=body.split(":");
     Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     String mobileNumber = keyValue[0].trim().replaceAll("\\{", "").replaceAll("\"", "");
     String url = keyValue[1].trim().replaceAll("\\}", "").replaceAll("\"", "");
-    try {
-      if (number != null && number.isEmpty() == false) {
-        boolean result = redisTemplate.delete(number);
-        if (result == false)
-          LOGGER.log(Level.INFO, "NUMBER NOT PRESENT IN REDIS");
-        else
-          LOGGER.log(Level.INFO, "NUMBER RECORD DELETED");
-      } else {
+    try{
+      if(number != null && number.isEmpty()==false) {
+        number=number.replaceAll("\"","");
+        boolean result=redisTemplate.delete(number);
+        if(result==false)
+          LOGGER.log(Level.INFO,number+" NUMBER NOT PRESENT IN REDIS");
+        else LOGGER.log(Level.INFO,number+" NUMBER RECORD DELETED");
+      }
+      else
+      {
         if (!Objects.nonNull(redisTemplate.opsForValue().get(mobileNumber))) {
-          redisTemplate.opsForValue().set(mobileNumber, url, 5, TimeUnit.MINUTES);
-          LOGGER.log(Level.INFO, "ADDED");
-        } else {
-          redisTemplate.opsForValue().set(mobileNumber, url, 5, TimeUnit.MINUTES);
-          LOGGER.log(Level.INFO, "UPDATED");
+          redisTemplate.opsForValue().set(mobileNumber, url);
+          LOGGER.log(Level.INFO,"ADDED "+mobileNumber);
+        }
+        else{
+          redisTemplate.opsForValue().set(mobileNumber, url);
+          LOGGER.log(Level.INFO,"UPDATED "+mobileNumber);
         }
       }
-    } catch (Exception e) {
+    }
+    catch(Exception e)
+    {
       LOGGER.log(Level.SEVERE, (Supplier<String>) e);
     }
   }
